effe8ed4c35ccc501d42c6966dadbb9574fecd54
zakero <zakero@noemail.net>
2020-07-17 18:53:52 +0000

    Primal Scream
    Added the library: Zakero_Profiler
    
    FossilOrigin-Name: 3c8629d39bc2ce68e0ec033602ad587ed825625378dec7e39d432dab6718fccd

---
+++ README.md
@@ -0,0 +1,24 @@
+# Zakero's C++ Header Libraries
+
+This is a collection of libraries in C++ header file form.  Due to their 
+nature, the libraries are not stand-alone _Single Header Libraries_.  The 
+reason for this is that there maybe dependencies between the headers.  Refer 
+to each library to learn what they depend on.  Or just add all of them to 
+your project and not worry about it.  :-)
+
+The documentation is available locally, if cloned, at `api/index.html` as 
+well as [online](https://zhl.zakero.com/api).  The official website for the 
+project is [zhl.zakero.com](https://zhl.zakero.com).
+
+| Library             | Description                                                       |
+|---------------------|-------------------------------------------------------------------|
+| Zakero_Profiler.h   | Generate profiling data that can be visualized in Chrome/Chromium |
+| _More coming soon_  |                                                                   |
+
+# License
+
+All libraries are licensed using the [Mozilla Public License v2](https://www.mozilla.org/en-US/MPL/2.0/) 
+([FAQ](https://www.mozilla.org/en-US/MPL/2.0/FAQ/)).  The key point of this 
+license is that the license applies __only__ to the header file, not what 
+includes it or what it is compiled into (not viral).
+
---
+++ test/Zakero_Profiler/Manual_-_Check_Output.cpp
@@ -0,0 +1,76 @@
+// g++ -std=gnu++20 -o Manual_-_Check_Output Manual_-_Check_Output.cpp
+
+#define ZAKERO_PROFILER_ENABLE
+#define ZAKERO_PROFILER_IMPLEMENTATION
+#include "../../include/Zakero_Profiler.h"
+
+void func1()
+{
+	ZAKERO_PROFILER("test", "empty")
+}
+
+void func2()
+{
+	ZAKERO_PROFILER("test", "work")
+
+	int total = 0;
+	for(int i = 1; i < 10; i++)
+	{
+		if(i % 2)
+		{
+			total -= i;
+		}
+		else
+		{
+			total += i;
+		}
+	}
+}
+
+void func3()
+{
+	ZAKERO_PROFILER("test", "layer")
+
+	//ZAKERO_PROFILER_DEACTIVATE
+
+	int total = 0;
+	for(int i = 1; i < 10; i++)
+	{
+		ZAKERO_PROFILER_INSTANT("test", "for_loop")
+
+		if(i % 2)
+		{
+			ZAKERO_PROFILER("test", "sub")
+			total -= i;
+		}
+		else
+		{
+			ZAKERO_PROFILER("test", "add")
+			total += i;
+		}
+	}
+
+	//ZAKERO_PROFILER_ACTIVATE
+}
+
+int main()
+{
+	const std::string TestName("Manual_-_Check_Output");
+	const std::string TestFile = TestName + ".json";
+
+	zakero::Profiler::MetaData meta_data =
+	{	{ "application" , TestName }
+	,	{ "version"     , "1.0.0" }
+	};
+	ZAKERO_PROFILER_INIT_METADATA(TestFile, meta_data);
+
+	func1();
+	func2();
+	func3();
+
+	std::cout << "1. Verify that \"" << TestFile << "\" contains data.\n";
+	std::cout << "2. Verify \"" << TestFile << "\" is viewable in a browser.\n";
+	std::cout << "   - As documented, Profile Timeline should have no errors.\n";
+
+	return 0;
+}

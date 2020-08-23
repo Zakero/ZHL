83f0f8be49ec38cd6f69ecc833d528e738292c40
zakero <zakero@noemail.net>
2020-08-23 21:17:36 +0000

    Updated the documentation
    FossilOrigin-Name: 99e3203427ee68518253980cb8e5c9531f22c501ca3cbbc6bddc3238210b7398

--- html/search/typedefs_0.js
+++ html/search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['addressmap_57',['AddressMap',['../classzakero_1_1_memory_pool.html#a5e9291872c2c7b3063a32d0a05408944',1,'zakero::MemoryPool']]]
+  ['addressmap_61',['AddressMap',['../classzakero_1_1MemoryPool.html#a5e9291872c2c7b3063a32d0a05408944',1,'zakero::MemoryPool']]]
 ];
